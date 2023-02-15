f6f32bf5d95fc0a55261a9b8085b1a573c600f30
Michael Hur <jhhur716@gmail.com>
2023-02-15 23:34:15 +0900

    feat: Amplify Setting 수정

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,28 +1,28 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "splitBillsApi": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
+    "api": {
+        "splitBillsApi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "function": {
+        "splitBillsLambda": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string",
+            "LambdaExecutionRoleArn": "string"
+        }
+    },
+    "storage": {
+        "groups": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
     }
-  },
-  "function": {
-    "splitBillsLambda": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
-  "storage": {
-    "groups": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
-    }
-  }
 }
